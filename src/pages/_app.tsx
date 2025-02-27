cdb105588c45e1df2d954a4d7a451ba633e5bd43
Hamzaach <hamza.rotf@gmail.com>
2025-02-28 00:13:31 +0100

    rm lenis

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,11 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
-import { useLenis } from "@/functions/useLenis";
 import Nav2 from "@/components/Nav2";
 import Footer from "@/components/Footer";
 import ScrollToTop from "@/components/ScrollToTop";
 import { ThemeProvider } from "@/context/ThemeContext";
 
 export default function App({ Component, pageProps }: AppProps) {
-  // useLenis(); 
 
   return (
     <>
