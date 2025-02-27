7e6be8ac329b85e7c4523e3d57e426113c42a865
Hamzaach <hamza.rotf@gmail.com>
2025-02-27 13:54:19 +0100

    gradient fix

--- src/components/Socials.tsx
+++ src/components/Socials.tsx
@@ -1,22 +1,32 @@
-
 import { FaGithub, FaInstagram, FaEnvelope } from "react-icons/fa";
 import styles from "@/styles/Home.module.css";
-import React from 'react'
+import React from "react";
 
 const Socials = () => {
   return (
     <div className={styles.socials}>
-        <a href="https://github.com/HamzaChl" target="_blank">
-          <FaGithub />
-        </a>
-        <a href="https://www.instagram.com/hamzaachl/" target="_blank">
-          <FaInstagram />
-        </a>
-        <a href="mailto:contact@exemple.com">
-          <FaEnvelope />
-        </a>
-      </div>
-  )
-}
+      {/* Définition du gradient */}
+      <svg width="0" height="0">
+        <defs>
+          <linearGradient id="gradient" x1="0%" y1="0%" x2="100%" y2="0%">
+            <stop offset="0%" style={{ stopColor: "#4568dc", stopOpacity: 1 }} />
+            <stop offset="100%" style={{ stopColor: "#b06ab3", stopOpacity: 1 }} />
+          </linearGradient>
+        </defs>
+      </svg>
 
-export default Socials
+      {/* Icônes avec couleur par défaut et gradient au hover */}
+      <a href="https://github.com/HamzaChl" target="_blank">
+        <FaGithub />
+      </a>
+      <a href="https://www.instagram.com/hamzaachl/" target="_blank">
+        <FaInstagram />
+      </a>
+      <a href="mailto:contact@exemple.com">
+        <FaEnvelope />
+      </a>
+    </div>
+  );
+};
+
+export default Socials;
